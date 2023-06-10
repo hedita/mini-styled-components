c7741705f51a2578b053c27570616da23dc643dc
Hedita <hedi.takrami@gmail.com>
2023-06-10 23:16:54 +0330

    ,

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import Input from "./Input";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button>Update results</Button>
+      <Input placeholder="Search by keywords" />
+    </ThemeProvider>
+  )
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: ${props => props.theme.color.tertiary};
+  color: white;
+  padding: ${props => props.theme.spacing.small};
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-right: ${props => props.theme.spacing.small};
+`;
+
+export default Button;
